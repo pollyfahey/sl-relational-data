fda0ba8c557524101528232b82228e7543a5cb61
pollyfahey <pollyfahey@gmail.com>
2018-12-20 15:06:06 +0000

    finished the create method

--- include/sl-relational-data.h
+++ include/sl-relational-data.h
@@ -2,6 +2,41 @@
 #include <stddef.h>
 #include <stdint.h>
 
+typedef struct slrdata_element_t slrdata_element_t;
+
+struct slrdata_element_t {
+	uint_fast64_t id;
+	unsigned char *label;
+	slrdata_element_t *element_next;
+};
+
+typedef struct slrdata_tuple_t slrdata_tuple_t;
+
+struct slrdata_tuple_t {
+	// pointer to an array of element ids
+	uint_fast64_t *elements;
+	slrdata_tuple_t *tuple_next;
+};
+
+typedef struct slrdata_relation_t slrdata_relation_t;
+
+struct slrdata_relation_t {
+	uint_fast64_t arity;
+	uint_fast64_t tuple_count;
+	const char *restrict label;
+	slrdata_tuple_t *tuples;
+	slrdata_relation_t *relation_next;
+};
+
+struct slrdata_create_t
+{
+	uint_fast64_t degree;
+	uint_fast64_t element_count;
+	uint_fast64_t relation_count;
+	slrdata_element_t *elements;
+	slrdata_relation_t *relations;
+};
+
 struct slrdata_t
 {
 	int fd;
@@ -11,63 +46,12 @@ struct slrdata_t
 };
 
 typedef struct slrdata_t slrdata_t;
-
-struct slrdata_element_t
-{
-	unsigned char *label[100];
-};
-
-
-typedef struct slrdata_element_t slrdata_element_t;
-typedef struct slrdata_elementlist_t slrdata_elementlist_t;
-
-struct slrdata_elementlist_t {
-	slrdata_element_t element_entry;
-	slrdata_elementlist_t *element_next;
-};
-
-struct slrdata_tuple_t
-{
-	slrdata_elementlist_t *elements;
-};
-
-typedef struct slrdata_tuple_t slrdata_tuple_t;
-
-struct slrdata_relation_t
-{
-
-	uint_fast64_t arity;
-	unsigned char *label[100];
-	slrdata_tuple_t *tuples[];
-};
-
-typedef struct slrdata_relation_t slrdata_relation_t;
-
-struct slrdata_create_t
-{
-	uint_fast64_t degree;
-	uint_fast64_t element_count;
-	uint_fast64_t relation_count;
-	slrdata_elementlist_t *elements;
-	slrdata_relation_t *relations[100];
-};
-
 typedef struct slrdata_create_t slrdata_create_t;
 
-int slrdata_open_or_create(slrdata_t *d, const char *restrict filename, bool readonly);
+int slrdata_open(slrdata_t *d, const char *restrict filepath, bool readonly);
 
 void slrdata_close(slrdata_t *d);
 
-int slrdata_create(slrdata_t *d, slrdata_create_t *c, const char *restrict filename);
-
-uint_fast64_t slrdata_number_of_elements(slrdata_t *d);
-
-uint_fast64_t slrdata_number_of_relations(slrdata_t *d);
-
-uint_fast64_t slrdata_element_label(slrdata_t *d, uint_fast64_t n);
-
-uint_fast64_t slrdata_relation_label(slrdata_t *d, uint_fast64_t n);
-
-uint_fast64_t slrdata_relation_arity(slrdata_t *d, uint_fast64_t n);
+int slrdata_create(slrdata_create_t *c, const char *restrict foldername);
 
 
--- src/sl-relational-data.c
+++ src/sl-relational-data.c
@@ -4,19 +4,21 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#include <stdlib.h>
 
-// Size of 'slrdata', version number
-#define SLRDATA_HEADERSIZE_BASIC 16
-// SLRDATA_HEADERSIZE_BASIC plus size of file size, elements and relations offsets
-#define SLRDATA_HEADERSIZE (SLRDATA_HEADERSIZE_BASIC + 3 * 8)
-#define SLRDATA_HEADER_ELEMENTSOFFSET (SLRDATA_HEADERSIZE_BASIC + 8)
-#define SLRDATA_HEADER_RELATIONSOFFSET (SLRDATA_HEADERSIZE_BASIC + 2 * 8)
+#define SLRDATA_HEADERSIZE_BASIC (3 * 8)
 
-#define SLRDATA_ELEMENTLIST_HEADERSIZE 12
-#define SLRDATA_ELEMENTSIZE 6
+#define SLRDATA_MAIN_HEADERSIZE (SLRDATA_HEADERSIZE_BASIC + 2 * 8)
+#define SLRDATA_MAIN_LISTHEADERSIZE 12
+#define SLRDATA_MAIN_ELEMENTSIZE 6
+#define SLRDATA_MAIN_RELATIONSIZE 6
 
-#define SLRDATA_RELATIONLIST_HEADERSIZE 12
-#define SLRDATA_RELATIONSIZE 20
+#define SLRDATA_RELATION_HEADERSIZE (SLRDATA_HEADERSIZE_BASIC + (2 * 8) + 6)
+#define SLRDATA_RELATION_LISTHEADERSIZE 12
+#define SLRDATA_RELATION_ELEMENTSIZE 8
+// size of tuple is SLRDATA_RELATION_TUPLEELEMENTSIZE * arity
+#define SLRDATA_RELATION_TUPLEELEMENTSIZE 6
+#define SLRDATA_RELATION_INCIDENCESIZE 6
 
 static void slrdata_write( uint_fast8_t bytes, void *ptr, uint_fast64_t v)
 {
@@ -36,7 +38,7 @@ static uint_fast64_t slrdata_read(uint_fast8_t bytes, const void *ptr)
 	return(ret);
 }
 
-// Write a new header (and empty element and relations lists)
+// Write a new basic header
 static void slrdata_headerinit(unsigned char *header, uint_fast64_t filesize)
 {
 	memcpy(header, u8"slrdata", 8);
@@ -46,133 +48,196 @@ static void slrdata_headerinit(unsigned char *header, uint_fast64_t filesize)
 
 	// Size
 	slrdata_write(8, header + SLRDATA_HEADERSIZE_BASIC, filesize);
-
-	// elements list and relations list offsets - 0 as there isn't any yet
-	slrdata_write(8, header + SLRDATA_HEADERSIZE_BASIC + 8, 0);
-	slrdata_write(8, header + SLRDATA_HEADERSIZE_BASIC + 16, 0);
 }
 
-static void slrdata_add_element_list(unsigned char *data, slrdata_t *d, slrdata_create_t *c, uint_fast64_t offset)
+static int slrdata_create_and_write_file(char* filepath, unsigned char* data, uint_fast64_t size)
 {
-	// update element list offset
-	slrdata_write(8, data + SLRDATA_HEADER_ELEMENTSOFFSET, offset);
+	struct slrdata_t d;
 
-	// write element list header
-	slrdata_write(6, data + offset, SLRDATA_ELEMENTSIZE * c->element_count);
-	slrdata_write(6, data + offset + 6, c->element_count);
-
-	// add elements TODO write actual label instead of number
-	for(uint_fast64_t i = 0; i < c->element_count; i++)
-	{
-		slrdata_write(6, data + offset + SLRDATA_ELEMENTLIST_HEADERSIZE + i * SLRDATA_ELEMENTSIZE, i + 1);
-	}
-}
-
-static void slrdata_add_relations_list(unsigned char *data, slrdata_t *d, slrdata_create_t *c, uint_fast64_t offset)
-{
-	// update relation list offset
-	slrdata_write(8, data + SLRDATA_HEADER_RELATIONSOFFSET, offset);
-
-	// write relation list header
-	slrdata_write(6, data + offset, SLRDATA_RELATIONSIZE * c->relation_count);
-	slrdata_write(6, data + offset + 6, c->relation_count);
-
-	// add relations
-	uint_fast64_t relations_offset = offset + SLRDATA_RELATIONLIST_HEADERSIZE;
-	for(uint_fast64_t i = 0; i < c->relation_count; i++)
-	{
-		// label TODO
-		slrdata_write(6, data + relations_offset + i * SLRDATA_RELATIONSIZE, i + 1);
-
-		// arity
-		slrdata_write(6, data + relations_offset + (i * SLRDATA_RELATIONSIZE) + 6, c->relations[i]->arity);
-
-		// element list offset
-		uint_fast64_t elements_offset = relations_offset + SLRDATA_RELATIONSIZE * c->relation_count + i * (SLRDATA_ELEMENTLIST_HEADERSIZE + 8 * c->element_count);
-		slrdata_write(8, data + relations_offset + i * SLRDATA_RELATIONSIZE + 12, elements_offset);
-
-		// element list
-		// header
-		slrdata_write(6, data + elements_offset, SLRDATA_ELEMENTSIZE * c->element_count);
-		slrdata_write(6, data + elements_offset + 6, c->element_count);
-		for(uint_fast64_t j = 0; j < c->element_count; j++)
-		{
-			// offset of list of tuples element j is in in relation i- initialise as 0
-			slrdata_write(8, data + elements_offset + SLRDATA_ELEMENTLIST_HEADERSIZE + j * 8, 0);
-		}
-	}
-}
-
-int slrdata_create(slrdata_t *d, slrdata_create_t *c, const char *restrict filename)
-{
-	// checks if the file already exists
-	if(access(filename, F_OK) != -1)
-		return(-1);
-
-	// create file
-	if((d->fd = open(filename, (O_RDWR | O_CREAT), S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)) == -1)
+	// create file - TODO change this to just create
+	if((d.fd = open(filepath, (O_RDWR | O_CREAT), S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)) == -1)
 			return(-1);
 
+	if(write(d.fd, data, size) == -1)
+	{
+		close(d.fd);
+		return(-2);
+	}
 
-	uint_fast64_t element_list_size = SLRDATA_ELEMENTLIST_HEADERSIZE + SLRDATA_ELEMENTSIZE * c->element_count;
-	uint_fast64_t relations_list_size = SLRDATA_RELATIONLIST_HEADERSIZE + SLRDATA_RELATIONSIZE * c->relation_count;
-	uint_fast64_t relations_elements_list_size = SLRDATA_ELEMENTLIST_HEADERSIZE + 8 * c->element_count;
+	close(d.fd);
+	return(0);
+}
 
-	uint_fast64_t size = SLRDATA_HEADERSIZE + element_list_size + relations_list_size + c->relation_count * relations_elements_list_size;
+static char* slrdata_filepath(const char *restrict foldername, const char *restrict filename)
+{
+	char *filepath = malloc(strlen(foldername) + strlen(filename) + 2 + 5);
+	filepath = strcpy(filepath, foldername);
+	filepath = strcat(filepath, "/");
+	filepath = strcat(filepath, filename);
+	filepath = strcat(filepath, ".sld");
+	return filepath;
+}
+
+static int slrdata_create_main_file(slrdata_create_t *c, const char *restrict foldername)
+{
+	uint_fast64_t element_list_size = SLRDATA_MAIN_LISTHEADERSIZE + (c->element_count * SLRDATA_MAIN_ELEMENTSIZE);
+	uint_fast64_t relation_list_size = SLRDATA_MAIN_LISTHEADERSIZE + (c->relation_count * SLRDATA_MAIN_RELATIONSIZE);
+
+	uint_fast64_t elements_offset = SLRDATA_MAIN_HEADERSIZE;
+	uint_fast64_t releations_offset = elements_offset + element_list_size;
+
+	uint_fast64_t size = SLRDATA_MAIN_HEADERSIZE + element_list_size + relation_list_size;
 	unsigned char data[size];
 
 	slrdata_headerinit(data, size);
 
-	slrdata_add_element_list(data, d, c, SLRDATA_HEADERSIZE);
+	// elements list and relations list offsets
+	slrdata_write(8, data + SLRDATA_HEADERSIZE_BASIC, elements_offset);
+	slrdata_write(8, data + SLRDATA_HEADERSIZE_BASIC + 8, releations_offset);
 
-	uint_fast64_t relations_offset = SLRDATA_HEADERSIZE + SLRDATA_ELEMENTLIST_HEADERSIZE + SLRDATA_ELEMENTSIZE * c->element_count;
-	slrdata_add_relations_list(data, d, c, relations_offset);
+	// write element list header
+	slrdata_write(6, data + elements_offset, SLRDATA_MAIN_ELEMENTSIZE * c->element_count);
+	slrdata_write(6, data + elements_offset + 6, c->element_count);
 
-	// write data
-	if(write(d->fd, data, size) == -1)
+	// write elements TODO write actual labels
+	for(uint_fast64_t i = 0; i < c->element_count; i++)
 	{
-		close(d->fd);
-		return(-2);
+		slrdata_write(6, data + elements_offset + SLRDATA_MAIN_LISTHEADERSIZE + (i * SLRDATA_MAIN_ELEMENTSIZE), i);
 	}
 
-	d->size = size;
+	// write relations list header
+	slrdata_write(6, data + releations_offset, SLRDATA_MAIN_RELATIONSIZE * c->relation_count);
+	slrdata_write(6, data + releations_offset + 6, c->relation_count);
 
-
-	if((d->ptr = mmap(0, d->size, (PROT_READ | PROT_WRITE), MAP_SHARED, d->fd, 0)) == MAP_FAILED)
+	// write relations TODO write actual labels
+	for(uint_fast64_t i = 0; i < c->relation_count; i++)
 	{
-		close(d->fd);
-		return(-6);
+		slrdata_write(6, data + releations_offset + SLRDATA_MAIN_LISTHEADERSIZE + (i * SLRDATA_MAIN_RELATIONSIZE), i);
+	}
+
+	if(slrdata_create_and_write_file(slrdata_filepath(foldername, "main"), data, size))
+	{
+		return(-1);
 	}
 
 	return(0);
 }
 
-int slrdata_open_or_create(slrdata_t *d, const char *restrict filename, bool readonly)
+static int slrdata_create_relation_file(slrdata_create_t *c, slrdata_relation_t *r, const char *restrict foldername)
+{
+	uint_fast64_t tuple_list_size = SLRDATA_RELATION_LISTHEADERSIZE + (r->tuple_count * r->arity * SLRDATA_RELATION_TUPLEELEMENTSIZE);
+	uint_fast64_t element_list_size = SLRDATA_RELATION_LISTHEADERSIZE + (c->element_count * SLRDATA_RELATION_ELEMENTSIZE);
+	uint_fast64_t incidence_list_size = SLRDATA_MAIN_LISTHEADERSIZE + (c->degree * SLRDATA_RELATION_INCIDENCESIZE);
+
+	uint_fast64_t tuple_list_offset = SLRDATA_RELATION_HEADERSIZE;
+	uint_fast64_t element_list_offset = tuple_list_offset + tuple_list_size;
+
+	uint_fast64_t size = SLRDATA_RELATION_HEADERSIZE + tuple_list_size + element_list_size + (c->element_count * incidence_list_size);
+	unsigned char data[size];
+
+	// basic header
+	slrdata_headerinit(data, size);
+
+	// additional header
+	slrdata_write(8, data + SLRDATA_HEADERSIZE_BASIC, tuple_list_offset);
+	slrdata_write(8, data + SLRDATA_HEADERSIZE_BASIC + 8, element_list_offset);
+	slrdata_write(6, data + SLRDATA_HEADERSIZE_BASIC + 16, r->arity);
+
+	// add tuple list header
+	slrdata_write(6, data + tuple_list_offset, r->tuple_count * r->arity * SLRDATA_RELATION_TUPLEELEMENTSIZE);
+	slrdata_write(6, data + tuple_list_offset + 6, r->tuple_count);
+
+	// add tuple list
+	slrdata_tuple_t *current_tuple = r->tuples;
+	for(uint_fast64_t i = 0; i < r->tuple_count; i++)
+	{
+		for(uint_fast64_t j = 0; j < r->arity; j++)
+		{
+			uint_fast64_t offset = tuple_list_offset
+					+ SLRDATA_RELATION_LISTHEADERSIZE
+					+ (i * r->arity * SLRDATA_RELATION_TUPLEELEMENTSIZE)
+					+ (j * SLRDATA_RELATION_TUPLEELEMENTSIZE);
+			slrdata_write(6, data + offset, (uint_fast64_t)current_tuple->elements[j]);
+		}
+
+		current_tuple = current_tuple->tuple_next;
+	}
+
+	// add element list header
+	slrdata_write(6, data + element_list_offset, c->element_count * SLRDATA_RELATION_ELEMENTSIZE);
+	slrdata_write(6, data + element_list_offset + 6, c->element_count);
+
+	// add element list - contains offsets to incidence lists
+	for(uint_fast64_t i = 0; i < c->element_count; i++)
+	{
+		uint_fast64_t incidence_list_offset = element_list_offset + element_list_size + (i * (SLRDATA_RELATION_LISTHEADERSIZE + (c->degree * SLRDATA_RELATION_INCIDENCESIZE)));
+		slrdata_write(8, data + element_list_offset + SLRDATA_RELATION_LISTHEADERSIZE + (i * SLRDATA_RELATION_ELEMENTSIZE), incidence_list_offset);
+
+		// incidence list header
+		slrdata_write(6, data + incidence_list_offset, c->degree * SLRDATA_RELATION_INCIDENCESIZE);
+		slrdata_write(6, data + incidence_list_offset + 6, 0);
+	}
+
+	// add incidence lists
+	slrdata_tuple_t *current_tuple_list = r->tuples;
+	for(uint_fast64_t i = 0; i < r->tuple_count; i++)
+	{
+		for(uint_fast64_t j = 0; j < r->arity; j++)
+		{
+			uint_fast64_t element = (uint_fast64_t)current_tuple_list->elements[j];
+			uint_fast64_t incidence_list_offset = slrdata_read(8, data + element_list_offset + SLRDATA_RELATION_LISTHEADERSIZE + (element * SLRDATA_RELATION_ELEMENTSIZE));
+			uint_fast64_t degree = slrdata_read(6, data + incidence_list_offset + 6);
+
+			slrdata_write(6, data + incidence_list_offset + 6, degree + 1);
+            slrdata_write(6, data + incidence_list_offset + SLRDATA_RELATION_LISTHEADERSIZE + (degree * SLRDATA_RELATION_INCIDENCESIZE), i);
+		}
+
+		current_tuple_list = current_tuple_list->tuple_next;
+	}
+
+	if(slrdata_create_and_write_file(slrdata_filepath(foldername, r->label), data, size))
+	{
+		return(-1);
+	}
+
+	return(0);
+}
+
+int slrdata_create(slrdata_create_t *c, const char *restrict foldername)
+{
+	// makes directory - errors if it already exists TODO check modes
+	if(mkdir(foldername, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH))
+		return(-1);
+
+	// create main files
+	if(slrdata_create_main_file(c, foldername))
+	{
+		return(-1);
+	}
+
+	// create relation files
+	slrdata_relation_t *current_relation = c->relations;
+	for(uint_fast64_t i = 0; i < c->relation_count; i++)
+	{
+		if(slrdata_create_relation_file(c, current_relation, foldername))
+			return(-1);
+
+		current_relation = current_relation->relation_next;
+	}
+
+	return(0);
+}
+
+int slrdata_open(slrdata_t *d, const char *restrict filepath, bool readonly)
 {
 	struct stat stat;
 
-	if((d->fd = open(filename, readonly ? O_RDONLY : (O_RDWR | O_CREAT), S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)) == -1)
+	if((d->fd = open(filepath, readonly ? O_RDONLY : O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)) == -1)
 		return(-1);
 
 	fstat(d->fd, &stat);
 
-	// If file is empty and not read only then create header
-	if(!stat.st_size && !readonly)
-	{
-		unsigned char header[SLRDATA_HEADERSIZE];
-
-		slrdata_headerinit(header, SLRDATA_HEADERSIZE);
-
-		// write header
-		if(write(d->fd, header, SLRDATA_HEADERSIZE) == -1)
-		{
-			close(d->fd);
-			return(-2);
-		}
-
-		d->size = SLRDATA_HEADERSIZE;
-	}
-	else if(stat.st_size < SLRDATA_HEADERSIZE_BASIC) // File is too small to be valid
+	if(stat.st_size < SLRDATA_HEADERSIZE_BASIC) // File is too small to be valid
 	{
 		close(d->fd);
 		return(-3);
@@ -214,36 +279,4 @@ void slrdata_close(slrdata_t *d)
 {
 	munmap(d->ptr, d->size);
 	close(d->fd);
-}
-
-// The following are for testing purposes
-
-uint_fast64_t slrdata_number_of_elements(slrdata_t *d)
-{
-	uint_fast64_t offset = slrdata_read(8, d->ptr + SLRDATA_HEADER_ELEMENTSOFFSET);
-	return slrdata_read(6, d->ptr + offset + 6);
-}
-
-uint_fast64_t slrdata_number_of_relations(slrdata_t *d)
-{
-	uint_fast64_t offset = slrdata_read(8, d->ptr + SLRDATA_HEADER_RELATIONSOFFSET);
-	return slrdata_read(6, d->ptr + offset + 6);
-}
-
-uint_fast64_t slrdata_element_label(slrdata_t *d, uint_fast64_t n)
-{
-	uint_fast64_t offset = slrdata_read(8, d->ptr + SLRDATA_HEADER_ELEMENTSOFFSET);
-	return slrdata_read(6, d->ptr + offset + SLRDATA_ELEMENTLIST_HEADERSIZE + n * SLRDATA_ELEMENTSIZE);
-}
-
-uint_fast64_t slrdata_relation_label(slrdata_t *d, uint_fast64_t n)
-{
-	uint_fast64_t offset = slrdata_read(8, d->ptr + SLRDATA_HEADER_RELATIONSOFFSET);
-	return slrdata_read(6, d->ptr + offset + SLRDATA_RELATIONLIST_HEADERSIZE + n * SLRDATA_RELATIONSIZE);
-}
-
-uint_fast64_t slrdata_relation_arity(slrdata_t *d, uint_fast64_t n)
-{
-	uint_fast64_t offset = slrdata_read(8, d->ptr + SLRDATA_HEADER_RELATIONSOFFSET);
-	return slrdata_read(6, d->ptr + offset + SLRDATA_RELATIONLIST_HEADERSIZE + n * SLRDATA_RELATIONSIZE + 6);
 }
